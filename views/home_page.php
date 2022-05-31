4ee2e6624ee64cdc79a1841fd820851c561ca246
Eleonore <eleonore.jens@gmail.com>
2022-05-31 20:48:00 +0200

    first commit

---
+++ views/home_page.php
@@ -0,0 +1,25 @@
+<!-- Exercice: copy of this template: https://nicepage.com/css-templates/preview/learn-cooking-online-90820?device=desktop -->
+
+<div class="container">
+    <h1>Learn <b>Cooking</b> Online</h1>
+    <p class="sous-titre">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+    <p class="link">Image from <a href="https://www.freepik.com/free-photos-vectors/food" class="link">Freepik</a></p>
+    <div class="box un">
+        <h2>1</h2>
+        <h3>Base Meals</3>
+        <p class="box">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <a href=# class="box">Learn more</a>
+    </div>
+    <div class="box deux">
+        <h2 class="deux">2</h2>
+        <h3>Knife Skills</3>
+        <p class="box">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <a href=# class="box">Learn more</a>
+    </div>
+    <div class="box trois">
+        <h2>3</h2>
+        <h3>Cooking Veggies</3>
+        <p class="box">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <a href=# class="box trois">Learn more</a>
+    </div>
+</div>
